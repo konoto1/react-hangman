353b3e4e77a64df1676a30f977642f12d03c0ea8
Tomas.K <tomas.konopackis@gmail.com>
2024-08-12 11:58:57 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Alphabet } from './data/components/Alphabet.jsx';
 import { words } from './data/words.js';
 
 function App() {
-  let word = '';
   const calc = Math.floor(Math.random() * words.length);
 
   return (
