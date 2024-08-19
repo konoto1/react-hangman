62b5de70cb569890eb53beb692d328a511c7d4a8
Tomas.K <tomas.konopackis@gmail.com>
2024-08-19 11:53:42 +0300

    updated with win/lose messages

--- src/data/components/Alphabet.jsx
+++ src/data/components/Alphabet.jsx
@@ -1,14 +1,16 @@
-import {useState } from "react";
+import {useEffect, useState } from "react";
 import  hangman  from '../../../public/hangman.webp';
 
 export function Alphabet(params) {  
     const {word, actWord} = params;
+    const [won, setWon] = useState('')
     const [win, setWin] = useState(0);
     const [lose, setLose] = useState(0);
     const [lifes, setLifes] = useState(6);
     const [guess, setGuess] = useState(word);
+    const [disable, setDisable] = useState(false);
     const keyboard = "QWERTYUIOPASDFGHJKLZXCVBNM".split('');
-    const btn = keyboard.map(symbol => <button style={{}} disabled={false} onClick={e => checkLetter(e)} key={symbol}>{symbol}</button>);
+    const btn = keyboard.map(symbol => <button style={{}} disabled={disable} onClick={e => checkLetter(e)} key={symbol}>{symbol}</button>);
     
 
     function checkLetter (e) {
@@ -27,8 +29,38 @@ export function Alphabet(params) {
         }  else {
                 e.target.style.backgroundColor = '#f27878';
                 setLifes(prev => prev - 1);
-        }  
-    }
+        };        
+    };
+
+    useEffect(checkWin, [guess]);
+    function checkWin () {
+        if (!guess.includes('_')) {
+            document.getElementById('root').style.backgroundColor = 'grey';
+            btn.map(() => setDisable(true));
+            setWin(prev => prev + 1);
+            setWon  (
+                <div className="won">
+                    <p>YOU WON!</p>
+                    <button>Play again</button>
+                </div>
+            );
+        };
+    };
+ 
+    useEffect(checkLose, [lifes]);
+    function checkLose () {
+        if (lifes === 0) {
+            document.getElementById('root').style.backgroundColor = 'grey';
+            btn.map(() => setDisable(true));
+            setLose (prev => prev + 1);
+            setWon  (
+                <div className="lose">
+                    <p>YOU LOSE!</p>
+                    <button>Play again</button>
+                </div>
+            );
+        };
+    };
 
     return   (
         <>
@@ -38,6 +70,7 @@ export function Alphabet(params) {
             <p>WIN: {win}</p>
             <p>LOSE: {lose}</p>
           </div>
+          {won}
           <img src={hangman} alt="" />
         </div>
         <p className='word'>{guess}</p>
